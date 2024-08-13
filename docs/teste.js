42545a45574d6ebb38d00ffea6ff6a97eb22e4c6
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-08-13 17:01:05 -0300

    new archive test

---
+++ docs/teste.js
@@ -0,0 +1,13 @@
+
+
+var comand1 = "Eu"
+var comand2 = "adoro"
+var comand3 = "churrasco"
+
+const teste = () => {
+    console.log(`${comand1} ${comand2} ${comand3}`);
+    
+
+}
+
+teste(); // node teste.js no cmd
